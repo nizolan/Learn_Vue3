d2db23bc6bb1c62501d1e8494b8994a3b7a89113
nizolan <nizolan@163.com>
2022-04-21 15:04:40 +0800

    update

---
+++ 10_组件化开发/component-b.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<h2>{{ title }}</h2>
+		<p>{{ desc }}</p>
+		<button @click="increate">点击按钮：{{ counter }}</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: "我是标题",
+				desc: "我是内容",
+				counter: 0,
+			};
+		},
+		methods: {
+			increate() {
+				return this.counter++;
+			},
+		},
+	};
+</script>
+
+<style scoped>
+</style>
